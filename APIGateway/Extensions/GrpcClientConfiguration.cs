a57b7653af500b3bbb88159710b67f951be315c7
Михаил <mixaila17may2003@gmail.com>
2024-11-20 23:41:42 +0400

    поправил конфиги

--- APIGateway/Extensions/GrpcClientConfiguration.cs
+++ APIGateway/Extensions/GrpcClientConfiguration.cs
@@ -7,8 +7,8 @@ public static class GrpcClientConfiguration
 {
     public static void ConfigureGrpcClients(this IServiceCollection services)
     {
-        ConfigureGrpcClient<ProfilePlayers.ProfilePlayersClient>(services, "PROFILE_BACKEND_URL");
-        ConfigureGrpcClient<ProfileAuth.ProfileAuthClient>(services, "PROFILE_BACKEND_URL");
+        ConfigureGrpcClient<ProfilePlayers.ProfilePlayersClient>(services, "PROFILE_SERVICE_URL");
+        ConfigureGrpcClient<ProfileAuth.ProfileAuthClient>(services, "PROFILE_SERVICE_URL");
     }
 
     private static void ConfigureGrpcClient<TClient>(IServiceCollection services, string environmentVariableName) where TClient : class
