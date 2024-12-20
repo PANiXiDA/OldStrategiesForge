d4aa532515a5032d73731ba08563e15eb6b60de1
Михаил <mixaila17may2003@gmail.com>
2024-12-20 17:44:53 +0400

    убрал закомменченный код миграций для докера

--- Services/ChatsService/DAL/Implementations/Extensions/DependencyInjection.cs
+++ Services/ChatsService/DAL/Implementations/Extensions/DependencyInjection.cs
@@ -8,22 +8,21 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration, string environment)
     {
-        services.AddDbContext<DefaultDbContext>(config => config.UseNpgsql(configuration["ConnectionStrings:DefaultConnectionString"]));
-        //if (environment == "Development")
-        //{
-        //    services.AddDbContext<DefaultDbContext>(config => config.UseNpgsql(configuration["ConnectionStrings:DefaultConnectionString"]));
-        //}
-        //else
-        //{
-        //    var connectionString =
-        //            $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
-        //            $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
-        //            $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" +
-        //            $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
-        //            $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+        if (environment == "Development")
+        {
+            services.AddDbContext<DefaultDbContext>(config => config.UseNpgsql(configuration["ConnectionStrings:DefaultConnectionString"]));
+        }
+        else
+        {
+            var connectionString =
+                    $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                    $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                    $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" +
+                    $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                    $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 
-        //    services.AddDbContext<DefaultDbContext>(options => options.UseNpgsql(connectionString));
-        //}
+            services.AddDbContext<DefaultDbContext>(options => options.UseNpgsql(connectionString));
+        }
 
         services.AddScoped<IChatsDAL, ChatsDAL>();
         services.AddScoped<IMessagesDAL, MessagesDAL>();
